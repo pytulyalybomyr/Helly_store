0e13e9ed02b4bb7fb8e56c3078c8ec20a9f61a25
pytulyalybomyr <pytulyalybomyr@gmail.com>
2023-11-30 14:37:27 +0200

    main

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,10 @@
 let burger__button = document.querySelector(".burger__button")
 let burger__block = document.querySelector(".burger__block")
+let toggle_delete = document.querySelector(".toggle_delete")
 
 burger__button.addEventListener("click", () => {
+    burger__block.classList.toggle('vision')
+})
+toggle_delete.addEventListener("click", () => {
     burger__block.classList.toggle('vision')
 })
